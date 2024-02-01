672b1d67f49092ec855e35274083cfc18e9d8cae
Cayden <114540789+caydenpark@users.noreply.github.com>
2024-02-01 14:08:41 -0700

    connect.js taken out of .gitignore

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 // express web server
-
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger-output.json');
 const express = require('express');
 const app = express();
+
+const contactRoutes = require('./routes/contacts.js');
 
 const routes = require('./routes/index.js');
 
 app.use(express.json());
 app.use('/', routes);
+app.use('/contacts', contactRoutes);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const port = process.env.PORT || 3000;
 
